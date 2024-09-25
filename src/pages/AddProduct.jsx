d52efd2877910b8fe4306cad865d538af01154d7
John Doe <mahdibenalaya566@gmail.com>
2024-09-25 20:16:45 +0100

    first commit

---
+++ src/pages/AddProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddProduct from '../components/AddProduct/AddProduct'
+
+function Addproduct() {
+  return (
+    <>
+    <AddProduct/>
+    </>
+  )
+}
+
+export default Addproduct
